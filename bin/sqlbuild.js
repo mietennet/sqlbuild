b443b1bc7e4f794d017f415256f0dad1d5263948
Karsten Pater <karsten@mieten.net>
2016-12-21 02:26:00 -0700

    basic feature complete

--- bin/sqlbuild.js
+++ bin/sqlbuild.js
@@ -1,28 +1,31 @@
 #! /usr/bin/env node
 
 const program = require('commander')
+const chalk = require('chalk')
+
 const pkg = require('../package.json')
-
 const sqlbuild = require('..')
-
-const path = require('path')
 
 program
   .version(pkg.version)
-  .usage('[options] <file ...>')
-  .arguments('<dir>')
-  .option('-1, --one-file', 'create all-in-one "schema.sql"-file')
-  .option('-s, --per-schema', 'create a schema.sql-file per "__init__.sql"-file ')
-  .option('-i, --init-filename [name]', 'Name of init-file [__init__.sql]', '__init__.sql')
-  .option('-f, --schema-filename [name]', 'Name of output-file [schemal.sql]', '__init__.sql')
-  .option('-w, --write-file', 'create "schema.sql"-file, without that flag it writes to stdout')
+  .usage('[options] <dir>')
+  // .arguments('<dir>')
+  .option('-v, --verbose', 'prints all filenames', false)
+  .option('-q, --quiet', 'prints no filenames', false)
+  .option('-d, --debug', 'prints debug info', false)
+
+  //  .option('-1, --one-file', 'create all-in-one "schema.sql"-file', false)
+  .option('-r, --recursive', 'create a schema.sql-file per "init.sql"-file in sub-dirs', false)
+
+  .option('-i, --init-filename [name]', 'Name of init-file [init.sql]', 'init.sql')
+  .option('-f, --schema-filename [name]', 'Name of output-file [schema.sql]', 'schema.sql')
+
+  .option('-w, --write-file', 'create "schema.sql"-file, without that flag it writes to stdout', false)
+
   .parse(process.argv)
 
-sqlbuild({
-  rootPath: 'sample/' || path.basename(process.cwd()),
-  oneFile: (program.oneFile),
-  writeFiles: (program.writeFile)
-}).then(function (res) {
-  if (res) console.log(res)
+sqlbuild(program).then(function () {
   return
+}).catch(function (e) {
+  console.error(chalk.bold.red(e.message))
 })
--- index.js
+++ index.js
@@ -3,45 +3,108 @@ const path = require('path')
 // import async from 'async'
 const _ = require('lodash')
 const globby = require('globby')
+const chalk = require('chalk')
+const v = require('voca')
 
 module.exports = function (options) {
-  // let logger = Logger({logLevel: logLevel || LogLevels.info})
+  const dir = (options.args[0] || '.')
+  if (options.debug) log('dir', dir)
+
+  function log (status, msg) {
+    if (!options.quiet || options.verbose || options.debug) {
+      status = status || ''
+      msg = msg || ''
+
+      // const toJson = (!!msg) && (msg.constructor === Object || msg.constructor === Array)
+      const toJson = typeof msg !== 'string'
+      msg = toJson ? '\n    ' + v.replaceAll(JSON.stringify(msg, null, '  '), '\n', '\n    ') + '\n' : msg
+
+      if ((status + msg) === '') {
+        console.log('')
+      } else {
+        if (v.includes(msg, '\n')) {
+          console.log('/*')
+          console.log('  ' + chalk.green(v.pad(status, 11)))
+          console.log(msg)
+          console.log('*/\n')
+        } else {
+          console.log('--' + chalk.green(v.pad(status, 11)) + msg)
+        }
+      }
+    }
+  }
+
+  function assert (val, msg) {
+    if (!val) throw new Error(msg)
+  }
 
   function buildOne (p, f, r) {
     f = path.join(p, f)
     p = path.dirname(f)
 
+    assert(fs.existsSync(f), 'File not found! ' + f)
+    if (options.verbose) log((r ? '+++++' : 'entry'), f)
+
     var data = fs.readFileSync(f, 'utf8')
-    var lines = data.split('\n')
+    var lines = data.trim().split('\n')
 
     for (var i = 0; i < lines.length; i++) {
       if (_.startsWith(lines[i], '\\ir')) {
         var incFile = path.join(p, lines[i].substr(4))
 
+        assert(fs.existsSync(incFile), incFile + ' File not found from ' + f)
+
+        if (fs.statSync(incFile).isDirectory()) {
+          incFile = path.join(incFile, options.initFilename)
+          assert(fs.existsSync(incFile), incFile + ' File not found from ' + f)
+        }
+
         lines[i] = buildOne(path.dirname(incFile), path.basename(incFile), true)
+      } else {
+        if (!options.writeFile) console.log(lines[i])
       }
     }
 
     const res = lines.join('\n')
 
-    if (!r && options.writeFiles) fs.writeFileSync(path.join(p, 'schema1.sql'), res, 'utf8')
+    if ((!r || (options.recursive && path.basename(f) === options.initFilename)) && options.writeFile) {
+      const outFile = path.join(p, 'schema1.sql')
+      fs.writeFileSync(outFile, res + '\n', 'utf8')
+      if (!options.quiet) {
+        log('write', outFile)
+        if (options.verbose) log()
+      }
+    }
 
     return res
   }
 
+  if (options.debug) log('options', options)
+
   const pattern = []
 
-  if (options.oneFile) {
-    pattern.push('__init__.sql')
-  } else {
-    pattern.push('**/__init__.sql')
-    pattern.push('!__init__.sql')
-  }
+  // if (options.oneFile) {
+  //   pattern.push(options.initFilename)
+  // } else {
+  //   pattern.push('**/' + options.initFilename)
+  //   pattern.push('!./' + options.initFilename)
+  // }
 
-  return globby(pattern, {cwd: options.rootPath}).then(function (files) {
+  pattern.push(options.initFilename)
+  if (options.debug) log('pattern', pattern)
+
+  return globby(pattern, {cwd: dir}).then(function (files) {
+    if (options.debug) log('entry file(s)', files)
+
+    log()
+
     const c = files.reduce(function (content, file) {
-      return options.writeFiles ? '' : content + buildOne(options.rootPath, file)
+      const nextContent = buildOne(dir, file)
+
+      return options.writeFile ? '' : content + nextContent
     }, '')
+
+    if (!options.verbose) log()
 
     return c
   })
