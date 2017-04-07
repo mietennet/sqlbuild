eb17f5dc29f4595c71183f3dc513cff627aa2d18
mietennet <karsten.pater@mieten.net>
2017-04-07 02:27:04 +0200

    added feature/execute

--- bin/sqlbuild.js
+++ bin/sqlbuild.js
@@ -3,6 +3,7 @@
 const program = require('commander')
 const chalk = require('chalk')
 const chokidar = require('chokidar')
+const shell = require('shelljs')
 
 const pkg = require('../package.json')
 const sqlbuild = require('..')
@@ -24,10 +25,19 @@ program
   .option('-w, --write-file', 'create "schema.sql"-file, without that flag it writes to stdout', false)
   .option('-W, --watch', 'watch for changes', false)
 
+  .option('-E, --exec [cmd]', 'excute command', false)
+
   .parse(process.argv)
 
 function run (changedFile) {
   sqlbuild(program, changedFile).then(function () {
+    if (program.exec) {
+      if (shell.exec(program.exec).code !== 0) {
+        console.error(chalk.bold.red('Error: execute failed'))
+        shell.exit(1)
+      }
+    }
+
     return
   }).catch(function (e) {
     console.error(chalk.bold.red(e.message))
@@ -37,16 +47,20 @@ function run (changedFile) {
 run()
 
 if (program.watch) {
-  const ignoredFiles = new RegExp([
-    'node_modules',
-    '.git',
-    (program.schemaFilename ? program.schemaFilename : undefined)
-  ].filter(function (e) { return e !== undefined }).join('|'), 'i')
+  const ignoredFiles = new RegExp(
+    [
+      'node_modules',
+      '.git',
+      (program.schemaFilename ? program.schemaFilename : undefined)
+    ]
+    .filter(function (e) { return e !== undefined })
+    .join('|')
+  , 'i')
 
   const watcher = chokidar.watch('**/*.sql', {
     ignored: ignoredFiles,
     persistent: true
   })
 
-  watcher.on('change', function (path) { run(path) })
+  watcher.on('change', run)
 }
